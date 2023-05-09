85f5cc85cb7c63b9dbfb16125b97b0616830b02c
kamaurra-headlight <101607079+kamaurra-headlight@users.noreply.github.com>
2023-05-09 12:28:03 -0700

    PD-129: initial parsing for json filters (#2)
    * unit tests included
    * package: version bump 1.0.4 -> 1.0.5

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ const assert = chai.assert;
 suite('Filter Stanza Parse', () =>
 {
     const parse = require('../source/Meadow-Filter').parse;
-    let queryStub = { addFilter: () => { }, setDistinct: () => { } };
+    let queryStub = { addFilter: () => { }, setDistinct: () => { }, addSort: () => { } };
     let queryMock;
 
     setup(() =>
@@ -121,5 +121,177 @@ suite('Filter Stanza Parse', () =>
 
         // then
         queryMock.verify();
+    });
+
+    suite('JSON filter stanzas', () =>
+    {
+        test('Filter by JSON - Bad Path', () =>
+        {
+            // given bad path (no dot notation at all)
+            const filterString = 'FBJV~FormData~EQ~123';
+
+            // when parse is called
+            const parseWrapper = () => { parse(filterString, queryStub) };
+
+            // then an error is thrown
+            expect(parseWrapper).to.throw();
+        });
+
+        test('[Security] Filter by JSON - Malicious Path', () =>
+        {
+            // given
+            const filterString = 'FBJV~FormData.IDWaffle\')) AND Select COUNT(*) From Document AND ((\'1~EQ~123';
+            queryMock.expects('addFilter').once().withArgs('','','(');
+            queryMock.expects('addFilter').once().withArgs('JSON_VALID(FormData)', 1, '=', 'AND');
+            //// make sure quotes are escaped to avoid command injections
+            queryMock.expects('addFilter').once().withArgs('JSON_UNQUOTE(JSON_EXTRACT(FormData, \'$.IDWaffle\\\')) AND Select COUNT(*) From Document AND ((\\\'1\'))', 123, '=', 'AND');
+            queryMock.expects('addFilter').once().withArgs('','',')');
+
+            // when
+            parse(filterString, queryStub);
+
+            // then
+            queryMock.verify();
+        });
+
+        test('Filter by JSON Value - EQ', () =>
+        {
+            // given
+            const filterString = 'FBJV~FormData.IDWaffle~EQ~123';
+            queryMock.expects('addFilter').once().withArgs('','','(');
+            queryMock.expects('addFilter').once().withArgs('JSON_VALID(FormData)', 1, '=', 'AND');
+            queryMock.expects('addFilter').once().withArgs('JSON_UNQUOTE(JSON_EXTRACT(FormData, \'$.IDWaffle\'))', 123, '=', 'AND');
+            queryMock.expects('addFilter').once().withArgs('','',')');
+
+            // when
+            parse(filterString, queryStub);
+
+            // then
+            queryMock.verify();
+        });
+
+        test('Filter by JSON Value - GT + OR', () =>
+        {
+            // given
+            const filterString = 'FBJVOR~FormData.IDWaffle~GT~123';
+            queryMock.expects('addFilter').once().withArgs('','','(');
+            queryMock.expects('addFilter').once().withArgs('JSON_VALID(FormData)', 1, '=', 'AND');
+            queryMock.expects('addFilter').once().withArgs('JSON_UNQUOTE(JSON_EXTRACT(FormData, \'$.IDWaffle\'))', 123, '>', 'OR');
+            queryMock.expects('addFilter').once().withArgs('','',')');
+
+            // when
+            parse(filterString, queryStub);
+
+            // then
+            queryMock.verify();
+        });
+
+        test('Filter by JSON List - INN', () =>
+        {
+            // given
+            const filterString = 'FBJL~SomeField.IDWaffle~INN~1,23,456';
+            queryMock.expects('addFilter').once().withArgs('','','(');
+            queryMock.expects('addFilter').once().withArgs('JSON_VALID(SomeField)', 1, '=', 'AND');
+            queryMock.expects('addFilter').once().withArgs('JSON_UNQUOTE(JSON_EXTRACT(SomeField, \'$.IDWaffle\'))', [ 1, 23, 456 ], 'IN', 'AND');
+            queryMock.expects('addFilter').once().withArgs('','',')');
+            // when
+            parse(filterString, queryStub);
+
+            // then
+            queryMock.verify();
+        });
+
+        test('Filter by JSON List - NIN', () =>
+        {
+            // given
+            const filterString = 'FBJL~SomeField.IDWaffle~NIN~1,23,456';
+            queryMock.expects('addFilter').once().withArgs('','','(');
+            queryMock.expects('addFilter').once().withArgs('JSON_VALID(SomeField)', 1, '=', 'AND');
+            queryMock.expects('addFilter').once().withArgs('JSON_UNQUOTE(JSON_EXTRACT(SomeField, \'$.IDWaffle\'))', [ 1, 23, 456 ], 'NOT IN', 'AND');
+            queryMock.expects('addFilter').once().withArgs('','',')');
+
+            // when
+            parse(filterString, queryStub);
+
+            // then
+            queryMock.verify();
+        });
+
+        test('Compound Filter with FBJLOR', () =>
+        {
+            // given
+            const filterString = 'FOP~0~(~0~FBV~Limit~GT~5~FBVOR~Limit~LT~0~FCP~0~)~0~FBJLOR~SomeField.IDWaffle~INN~1,23,456';
+            //// calls around non-json fields
+            queryMock.expects('addFilter').once().withArgs('', '', '(');
+            queryMock.expects('addFilter').once().withArgs('Limit', '5', '>', 'AND');
+            queryMock.expects('addFilter').once().withArgs('Limit', '0', '<', 'OR');
+            queryMock.expects('addFilter').once().withArgs('', '', ')');
+            //// calls around json fields
+            queryMock.expects('addFilter').once().withArgs('', '', '(');
+            queryMock.expects('addFilter').once().withArgs('JSON_VALID(SomeField)', 1, '=', 'AND');
+            queryMock.expects('addFilter').once().withArgs('JSON_UNQUOTE(JSON_EXTRACT(SomeField, \'$.IDWaffle\'))', [ 1, 23 ,456 ], 'IN', 'OR');
+            queryMock.expects('addFilter').once().withArgs('', '', ')');
+
+            // when
+            parse(filterString, queryStub);
+
+            // then
+            queryMock.verify();
+        });
+
+        test('Filter by JSON Date - LE', () =>
+        {
+            // given
+            const filterString = 'FBJD~FormData.Meta.ApprovalDate~LE~2019-12-07';
+            queryMock.expects('addFilter').once().withArgs('','','(');
+            queryMock.expects('addFilter').once().withArgs('JSON_VALID(FormData)', 1, '=', 'AND');
+            // ASSUMPTION: use of a list for the LE operation. This library assumes that retold(foxhound) will handle stripping the list away in this case
+            queryMock.expects('addFilter').once().withArgs('DATE(JSON_UNQUOTE(JSON_EXTRACT(FormData, \'$.Meta.ApprovalDate\')))', [ '2019-12-07' ], '<=', 'AND');
+            queryMock.expects('addFilter').once().withArgs('','',')');
+
+            // when
+            parse(filterString, queryStub);
+
+            // then
+            queryMock.verify();
+        });
+
+        test('Filter Sort by JSON Field (default: string)', () =>
+        {
+            // given
+            const filterString = 'FSJF~FormData.Meta.JCSequenceNumber~DESC~0';
+            queryMock.expects('addFilter').once().withArgs('JSON_VALID(FormData)', 1, '=', 'AND');
+            queryMock.expects('addSort').once().withArgs({ Column: 'CAST(FormData ->> \'$.Meta.JCSequenceNumber\' AS CHAR)', Direction: 'Descending'});
+
+            // when
+            parse(filterString, queryStub);
+
+            // then
+            queryMock.verify();
+        });
+
+        const fsjfByNumberTests =
+        [
+            { requestedType: 'UINT', expectedType: 'UNSIGNED'},
+            { requestedType: 'SINT', expectedType: 'SIGNED'},
+            { requestedType: 'DD', expectedType: 'DECIMAL(10,5)'},
+            { requestedType: 'DATE', expectedType: 'DATE'},
+        ];
+        fsjfByNumberTests.forEach((input) =>
+        {
+            test('Filter Sort by JSON Field (number)', () =>
+            {
+                // given
+                const filterString = `FSJF~FormData.Meta.JCSequenceNumber~ASC~${input.requestedType}`;
+            queryMock.expects('addFilter').once().withArgs('JSON_VALID(FormData)', 1, '=', 'AND');
+                queryMock.expects('addSort').once().withArgs({ Column: `CAST(FormData ->> \'$.Meta.JCSequenceNumber\' AS ${input.expectedType})`, Direction: 'Ascending'});
+
+                // when
+                parse(filterString, queryStub);
+
+                // then
+                queryMock.verify();
+            });
+        });
     });
 });
